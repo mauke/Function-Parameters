0d87c016a41b5c79531b89496bd20f408a44c840
Lukas Mai <l.mai@web.de>
2012-11-04 03:01:40 +0100

    un-C< > level 2 headings

--- lib/Function/Parameters/Info.pm
+++ lib/Function/Parameters/Info.pm
@@ -75,33 +75,33 @@ L<C<Function::Parameters::info>|Function::Parameters/Introspection> returns
 objects of this class to describe parameter lists of functions. The following
 methods are available:
 
-=head2 C<< $info->invocant >>
+=head2 $info->invocant
 
 Returns the name of the variable into which the first argument is
 L<C<shift>|perlfunc/shift>ed into automatically, or C<undef> if no such thing
 exists. This will usually return C<'$self'> for methods.
 
-=head2 C<< $info->positional_required >>
+=head2 $info->positional_required
 
 Returns a list of the names of the required positional parameters (or a count
 in scalar context).
 
-=head2 C<< $info->positional_optional >>
+=head2 $info->positional_optional
 
 Returns a list of the names of the optional positional parameters (or a count
 in scalar context).
 
-=head2 C<< $info->named_required >>
+=head2 $info->named_required
 
 Returns a list of the names of the required named parameters (or a count
 in scalar context).
 
-=head2 C<< $info->named_optional >>
+=head2 $info->named_optional
 
 Returns a list of the names of the optional named parameters (or a count
 in scalar context).
 
-=head2 C<< $info->slurpy >>
+=head2 $info->slurpy
 
 Returns the name of the final array or hash that gobbles up all remaining
 arguments, or C<undef> if no such thing exists.
@@ -110,14 +110,14 @@ As a special case, functions defined without an explicit parameter list (i.e.
 without C<( )>) will return C<'@_'> here because they accept any number of
 arguments.
 
-=head2 C<< $info->args_min >>
+=head2 $info->args_min
 
 Returns the minimum number of arguments this function requires. This is
 computed as follows: Invocant and required positional parameters count 1 each.
 Optional parameters don't count. Required named parameters count 2 each (key +
 value). Slurpy parameters don't count either because they accept empty lists.
 
-=head2 C<< $info->args_max >>
+=head2 $info->args_max
 
 Returns the maximum number of arguments this function accepts. This is computed
 as follows: If there is any named or slurpy parameter, the result is C<Inf>.
