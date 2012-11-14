e85d37cfe365abbbcd8f931bf4ee464b0b95a8f9
Lukas Mai <l.mai@web.de>
2012-11-14 18:33:25 +0100

    version bump (developer release)

--- lib/Function/Parameters.pm
+++ lib/Function/Parameters.pm
@@ -8,7 +8,9 @@ use Carp qw(confess);
 
 use XSLoader;
 BEGIN {
-	our $VERSION = '1.00';
+	our $VERSION = '1.00_02';
+	our $XS_VERSION = $VERSION;
+	$VERSION = eval $VERSION;
 	XSLoader::load;
 }
 
--- lib/Function/Parameters/Info.pm
+++ lib/Function/Parameters/Info.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use Moo;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 {
 	package Function::Parameters::Param;
