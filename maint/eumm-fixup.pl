1a304077ad2fc1464fc1a369097438fb3fc6e284
Lukas Mai <l.mai@web.de>
2015-02-23 08:50:56 +0100

    probe for -fsanitize support

--- maint/eumm-fixup.pl
+++ maint/eumm-fixup.pl
@@ -1,16 +1,38 @@
 use strict;
 use warnings;
 
+use Config ();
+
+my @ccflags;
+my @otherldflags;
+
+if (-e '/dev/null') {
+    my $good_cc_flag = sub {
+        system("echo 'int main(void) { return 0; }' | \Q$Config::Config{cc}\E @_ -xc - -o /dev/null") == 0
+    };
+    for my $flag (qw(-fsanitize=address -fsanitize=undefined)) {
+        if (!$good_cc_flag->($flag)) {
+            warn "!! Your C compiler ($Config::Config{cc}) doesn't seem to support '$flag'. Skipping ...\n";
+            next;
+        }
+        push @ccflags,      $flag;
+        push @otherldflags, $flag;
+    }
+}
+
 sub {
     my ($opt) = @_;
 
-    $opt->{postamble}{text} = <<'EOT';
+    $opt->{postamble}{text} .= <<"EOT";
 export RELEASE_TESTING=1
 export HARNESS_OPTIONS=c
 
-CCFLAGS += -DDEVEL -fsanitize=address -fsanitize=undefined
-OTHERLDFLAGS +=    -fsanitize=address -fsanitize=undefined
+CCFLAGS +=      @ccflags -DDEVEL
+OTHERLDFLAGS += @otherldflags
 
+EOT
+
+    $opt->{postamble}{text} .= <<'EOT';
 .PHONY: multitest
 multitest:
 	f=''; k=''; \
